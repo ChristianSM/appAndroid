4336d929f5079e5d8402221b8b6475700a505c83
ChristianSM <blakipirill.santos@gmail.com>
2023-09-19 18:54:36 +0200

    subida antes de Jetpack

--- app/src/main/java/com/example/appprueba/MainActivity.java
+++ app/src/main/java/com/example/appprueba/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.appprueba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    VideoView video;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        video = (VideoView) findViewById(R.id.videoView);
+        video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.atom));
+
+        MediaController mediaController = new MediaController(this);
+        video.setMediaController(mediaController);
+
+        video.start();
+
+        Button botonNavigation = (Button) findViewById(R.id.buttonSegundaPantalla);
+        botonNavigation.setOnClickListener(new OnClickListener);
+
+
+    }
+
+    public void irSegundaPantalla(View vista){
+        Intent intent = new Intent(MainActivity.this, SegundaActivity.class);
+        startActivity(intent);
     }
 }
